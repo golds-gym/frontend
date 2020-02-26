8e0a328ab27aa9210b24c172ded71758c24acf5e
Mikalai Davydzenka <mdavydzenka@exadel.com>
2020-02-27 00:40:02 +0300

    added login component

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <img alt="gold's gym logo" src="../assets/logo.png">
+    <Login msg="Welcome to Gold's GYM App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import Login from '@/components/Login.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Login
+  }
+}
+</script>
